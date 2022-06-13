3018d9b07b08f3f2f21a9e0a5aad91c59e3efd50
MengChun <adampig2010@gmail.com>
2022-06-13 18:25:49 +0800

    Refactor: class/id naming refactor

--- Week-2/Assignment-4/app.js
+++ Week-2/Assignment-4/app.js
@@ -1,13 +1,10 @@
 var welcomeBlock = document.querySelector(".welcome");
 
 welcomeBlock.addEventListener('click', () => {
-    welcomeBlock.innerHTML = "<h2>Have a Good Time!</h2>";
+    welcomeBlock.innerHTML = `<h2 style="padding: 3em 0; margin: 0px;">Have a Good Time!</h2>`;
     document.querySelector(".welcome").style.cursor = "default";
 });
 
 function showTheCourse() {
-    document.querySelector(".row4").style.display = "flex";
-}
-
-
-
+    document.querySelector(".course-content").style.display = "flex";
+};
