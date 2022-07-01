5a1e5c99d6ad92b10cd48009230f638b192dde1a
MengChun <adampig2010@gmail.com>
2022-07-01 16:50:45 +0800

    refactor: more understandable

--- Week-4/Assignment-4/app.js
+++ Week-4/Assignment-4/app.js
@@ -1,6 +1,6 @@
 function delayedResultPromise(n1, n2, delayTime) {
     const myPromise = new Promise( (resolve, reject) => {
-        setTimeout( () => {resolve(n1 + n2);}, delayTime);
+        setTimeout( () => resolve(n1 + n2), delayTime);
     });
     return myPromise
 }
@@ -8,7 +8,8 @@ delayedResultPromise(4, 5, 3000).then(console.log);
 // // 9 (4+5) will be shown in the console after 3 seconds
 
 async function main() {
-    console.log(await delayedResultPromise(4, 5, 3000));
+    const result = await delayedResultPromise(7, 8, 1000);
+    console.log(result);
 }
     
 main(); // result will be shown in the console after <delayTime> seconds
